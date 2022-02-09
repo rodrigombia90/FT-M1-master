ded9155c52bc8c1e6b066b3e1f9d9d964a60fff0
Rodrigombia90 <rodrigoalejandroloza11@gmail.com>
2022-02-08 21:58:47 -0300

    ED3

--- 05-EstructuraDeDatos-III/homework/homework.js
+++ 05-EstructuraDeDatos-III/homework/homework.js
@@ -52,100 +52,154 @@ anchura:
 va ordenando el primero de cada nivel empezando de ziquierda a derecha
 */
 
-
-function BinarySearchTree(valor) {
-
-  this.left=null;
-  this.right=null;
-  this.value=valor;
+/*
+al hacerlo recursivo, se abren contextos de ejecucion, desde el primero al ultimo, entonces el primero empieza y es el ultimo en cerrarse. El ultimo en abrirse es el primero en cerrarse
+*/
+function BinarySearchTree(value) {
+/*
+el tree tiene que tener tres propiedades*/
+this.value = value; // ID
+this.left=null;
+this.right=null;
 }
 
-BinarySearchTree.prototype.size = function(valor){
-  //tengo dos hijos
-  if(this.left && this.right) return this.left.size() + this.right.size()
-  //tengo hijo izquierdo
-  if(this.left) return this.left.size()+1
-  //tengo hijo derecho
-  if(this.right) return this.right.size()+1
-  return 1
-}
 
+ /*
 BinarySearchTree.prototype.insert = function(valor){
-  if(this.value > valor){
-    //izquierda
-    //si tengo un hijo izquierdo
+   /*if(this.value<valor){
     if(this.left){
-      return this.left.insert(valor)
+      this.insert(valor)
     }
-    else{
-      //si no lo tengo voy a hacer otro arbol
-      this.left=new BinarySearchTree(valor)
+  }else{
+    this.left = new BinarySearchTree(valor)
+  } */
+//}
+
+
+BinarySearchTree.prototype.insert = function(value){
+ if(value<this.value){
+   if(this.left){
+     this.left.insert(value)
+   }
+   else this.left = new BinarySearchTree(value)
+ }
+ else if(value>this.value){
+   if(this.right){
+     this.right.insert(value)
+   }
+   else this.right =new BinarySearchTree(value)
+ }
+}
+//choreada de nico
+BinarySearchTree.prototype.size = function(){
+  //Nodo hoja (BST)
+  if (!this.left && !this.right) return 1;
+  //Nodo con un solo hijo (BST)
+  if (!this.left) return 1 + this.right.size();
+  if (!this.right) return 1 + this.left.size();
+  //Nodo con ambos hijos
+  return 1 + this.left.size() + this.right.size();
+}
+
+/*ejemplo del stand up
+BinarySearchTree.prototype.size = function (){
+  // CASO BASE
+  if(this.right === null && this.left === null) return 1;
+  if(this.left !== null && this.right === null) return 1 + this.left.size();
+  if(this.right !== null && this.left === null) return 1 + this.right.size();
+  if(this.right !== null && this.left !== null) return 1 + this.left.size() + this.right.size();
+};
+*/
+
+
+//lo primero es saber si el valor que me llega esta dentro del arbol
+//despues pregunto si es menor el valor va a ir al a izquierda, si es mayor va a la derecha
+
+BinarySearchTree.prototype.contains = function(value){ //10
+  if(this.value===value) return true;//si el primero es el que me pasan, retorno
+  //y sino empiezo a la izquierda
+  if(value<this.value){
+    if(!this.left){//no va a la izquierda al pedo
+      return false;
     }
+    else {
+      return this.left.contains(value);
+    }    
   }
   else{
-    //derecha
-    if(this.right){
-      this.right.insert(valor)
+    if(!this.right){
+      return false;
     }
-    else{
-      this.right = new BinarySearchTree(valor)
-    }
-  }
-}
-
-//la linea 72 setea por defecto la forma in-order por si no viene por parametro en la variable order
-BinarySearchTree.prototype.depthFirstForEach = function(cb,order){
-  order = order || 'in-order'//
-  if(order=== 'pre-order'){
-    //valor, hijo izquierdo, derecho
-    cb(this.value)
-    if(this.left) this.left.depthFirstForEach(cb,order)
-    if(this.right) this.right.depthFirstForEach(cb,order)
-  }
-  if(order=== 'post-order'){
-    //hijo izquierdo, derecho, valor
-   if(this.left) this.left.depthFirstForEach(cb,order)
-  
-  if(this.right) this.right.depthFirstForEach(cb,order)
-  cb(this.value)
-  }
-  if(order === 'in-order'){
-  if(this.left) this.left.depthFirstForEach(cb,order)
-  cb(this.value)
-  if(this.right) this.right.depthFirstForEach(cb,order)
-  }
-
-}
-
-BinarySearchTree.prototype.breadthFirstForEach = function(cb,elementsToExecute){
-  var elementsToExecute = elementsToExecute || []
-  cb(this.value)
-    if(this.left) elementsToExecute.push(this.left)
-    if(this.right) elementsToExecute.push(this.right)
-  
-  //aca en esta linea llamamos a la funcion, pero vamos pusheadno los nodos en orden de jerarquita, se van guardando en elementsToExecute)
-  let siguienteNodo = elementsToExecute.shift()
-  if(siguienteNodo){
-  siguienteNodo.breadthFirstForEach(cb,elementsToExecute)
+    else {
+      return this.right.contains(value);
+    }    
   }
 }
 
 
-BinarySearchTree.prototype.contains = function(valor){
-if(this.value  === valor) return true
-//si llego hasta aca el valor no es igual, y hay que empezar a buscar
-  if(valor < this.value){
-    if(this.left){
-      return this.left.contains(valor)    
-  }
-  else{
-    if(this.right){
-      return this.right.contains(valor)
-    }
-  }
-  return false
+BinarySearchTree.prototype.depthFirstForEach = function (cb,order){
+
 }
+
+//le paso por parametro uin array. esto se puede hacer con array s y objetos. cuando la llamas por priemra vez
+//si lo llamo sin parametro, usa el que le mando, (a=[]), y usa ese, y si le pongo parametro, usa el que le pongo
+//al crear un array, creo algo fisico y estatico en memoria, en lugar de algo dinamico y que cambia en los distintos contextos de ejecucion
+
+
+/*BinarySearchTree.prototype.size = function (a=[]){
+  //* si esta linea va aca: pre
+  if(this.left) this.left.size(a)
+    a.push(this.value) //* si esta linea va aca: en orden
+    if(this.rigth) this.rigth.size(a)
+    return (a.length)
+    //* si esta linea va aca: pos
+}*/
+//recorre todo el abrol de la parte mas baja de la izquierda y de ahi va a la derecha, y despues empieza a devolver todo
+
+
+/*
+la funcoin cb sirve para pushear los elementos que estan en el test
+el array es un aux que usamos para guardar punteros a los objetos con los que vamos a tener que seguir trabajando
+lo primero que hace es preguntar si tiene un hijo izquierdo, guarda un puntero en el arr aux, se guarda un punteor a ese hijo izuqierdo
+despues pregunta si tiene un hijo derecho
+despues llamo a la cb, y ahi le asigno al arr del test
+y despues pregunto si el arr tiene valores
+
+
+*/
+
+
+
+BinarySearchTree.prototype.breadthFirstForEach = function(cb,a=[]){
+ if(this.left) a.push(this.left)
+ if(this.right) a.push(this.right)
+ cb(this.value)
+ if(a.length){
+   var aux=a[0]//aca se crea la variable aux, que tome el puntero que estaba en el primer elemento del array
+   //se guarda ese puntero en aux, despues se elimina el valor que guarde en aux y ese valor se lo vuelvo a pasar a la funcion recursivamente desde el primer valor que guarde
+   //en la linea 172
+   a.shift()
+   aux.breadthFirstForEach(cb,a); 
+ }
 }
+
+/*
+BinarySearchTree.prototype.size = function(valor){
+  //si tengo hijo izquierdo e hijo derecho
+  //return de left.size() + right.size(), si no tengo ninguno de los dos retorno 1
+
+}
+
+BinarySearchTree.protoype.deepFirstForEach = function(cb,order){
+  //si tengo hijo izquierdo, llamo a la funcion this.left.deepFirstForEach()
+  //cd(this.value)
+  //si tengo hijo derecho, this.right.deepFirst.ForEach
+}*/
+
+//BinarySearchTree.protoype.breadthFirstForEach = function(cb){
+ // tener una estructura de datos alternativa, como un array, guardar los datos en el array para ver cuales son izquierdos y cuales son derechos
+
+//} 
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
