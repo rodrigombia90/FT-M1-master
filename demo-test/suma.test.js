e8e00897879c187873cf804bdb89126df5fc9a35
rodrigombia90 <67389454+rodrigombia90@users.noreply.github.com>
2022-02-10 18:26:01 -0300

    test
    test

---
+++ demo-test/suma.test.js
@@ -0,0 +1,10 @@
+const suma = require("./suma")
+
+describe('La funcion suma deberia',function (){
+    //los describe van a agrupar testing
+
+    it('devolver 4 cuando la llame con 2 y 2',function(){
+        expect(true)//el resultado de la funcion
+        .toBe(true)//el valor que deberia devolver
+    })// los it van a ser de correr un test en particular
+})
